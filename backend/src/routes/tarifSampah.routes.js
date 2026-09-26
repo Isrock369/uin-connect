e8d079245c6f6b60e0155e375e601f9948476c91
Isrock369 <moh.isrock182@gmail.com>
2026-09-26 14:31:16 +0700

    Add CRUD routes for tarifSampah

--- backend/src/routes/tarifSampah.routes.js
+++ backend/src/routes/tarifSampah.routes.js
@@ -3,6 +3,8 @@ const ctrl = require('../controllers/tarifSampah.controller')
 const { requireAuth } = require('../middleware/auth')
 
 router.get('/', ctrl.getAll) // publik, dipakai Portal Santri
+router.post('/', requireAuth, ctrl.create)
 router.put('/:id', requireAuth, ctrl.update)
+router.delete('/:id', requireAuth, ctrl.remove)
 
 module.exports = router
